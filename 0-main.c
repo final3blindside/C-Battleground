6537c5e9f26ec5b2479f780c6189c2508fa8c339
final3blindside <72025445+final3blindside@users.noreply.github.com>
2022-12-25 22:08:26 +0800

    learn-c.org exercises

--- 0-main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int(){
-    printf("Hi I am final3blindside!");
-    printf("Hi I am Yuan")
-}
---
+++ arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int grades[3];
+    int average;
+
+    grades[0] = 80;
+    grades[1] = 85;
+    grades[2] = 90;
+
+    average = (grades[1] + grades[0] + grades[2]) / 3;
+    printf("The average of the three numbers is:  %d", average)
+    return 0;
+
+}
+
+main();
