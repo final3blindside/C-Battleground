88bbc986ab684ea72a661e23da409f17d9d75ffc
final3blindside <72025445+final3blindside@users.noreply.github.com>
2022-12-25 21:57:08 +0800

    learn-c.org exercises

---
+++ 0-main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int(){
+    printf("Hi I am final3blindside!");
+    printf("Hi I am Yuan")
+}
--- variables-and-types.c
+++ variables-and-types.c
@@ -11,3 +11,11 @@ int main() {
   printf("The sum of a, b, and c is %f.", sum);
   return 0;
 }
+
+------------------------------------------------
+
+// C does not have a Boolean type, so we have to define.
+
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
