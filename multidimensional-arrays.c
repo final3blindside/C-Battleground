864a88fc944dfbadbba5378697a20656e07c2c2e
final3blindside <72025445+final3blindside@users.noreply.github.com>
2022-12-27 21:27:27 +0800

    learn-c.org exercises

--- multidimensional-arrays.c
+++ multidimensional-arrays.c
@@ -1,7 +1,48 @@
-// When initializing multidimensional arrays, rows come first, and then columns
+// When initializing multidimensional arrays, rows come first, and then columns second
+// Example: int a[4][2]; has 4 rows and 2 columns.
+
+
+/*
+Exercise:
+Let us try to find out the average marks of a group of five students for two subjects, Mathematics and Physics. To do this, we use a two-dimensional array called grades. The marks corresponding to Mathematics would be stored in the first row (grades[0]), whereas those corresponding to Physics would be stored in the second row (grades[1]). Complete the following steps so that you can execute this program.
+
+- Declare grades as a two-dimensional array of integers
+- Complete the for loops by specifying their terminating conditions
+- Compute the average marks obtained in each subject
+
+*/
 
 #include <stdio.h>
 
-int main(){
+int main() {
+    // TODO: Declare grades as a two-dimensional array of integers
+    int grades[2][5];
+    float average;
+    int i;
+    int j;
 
+    grades[0][0] = 80;
+    grades[0][1] = 70;
+    grades[0][2] = 65;
+    grades[0][3] = 89;
+    grades[0][4] = 90;
+
+    grades[1][0] = 85;
+    grades[1][1] = 80;
+    grades[1][2] = 80;
+    grades[1][3] = 82;
+    grades[1][4] = 87;
+
+    /* TODO: complete the for loop with appropriate terminating conditions */
+    for (i = 0; i < ; i++) {
+        average = 0;
+        for (j = 0; j < ; j++) {
+            average += grades[i][j];
+        }
+
+        /* TODO: compute the average marks for subject i */
+        printf("The average marks obtained in subject %d is: %.2f\n", i, average);
+    }
+
+    return 0;
 }
